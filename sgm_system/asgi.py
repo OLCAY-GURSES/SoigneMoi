6dcacb61590eff5bc196c96f6fc402072bcf5957
OLCAY-GURSES <OL.GURSES@GMAIL.COM>
2024-02-12 11:33:39 +0100

    add Hospital views ands templates

--- sgm_system/asgi.py
+++ sgm_system/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for manage project.
+ASGI config for sgm_system project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manage.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sgm_system.settings')
 
 application = get_asgi_application()
