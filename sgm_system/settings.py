2d758863be258b684503798fb6d003dce82f3973
OLCAY-GURSES <OL.GURSES@GMAIL.COM>
2024-02-23 10:33:27 +0100

    Add DRF

--- sgm_system/settings.py
+++ sgm_system/settings.py
@@ -103,7 +103,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+REST_FRAMEWORK = {
+  'DEFAULT_PERMISSION_CLASSES': [
+      'rest_framework.permissions.IsAuthenticated'
+  ]
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
