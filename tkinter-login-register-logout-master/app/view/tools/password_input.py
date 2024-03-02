df1aad7df2ea243e5b52c9b79e4a1af03a67d0ae
OLCAY-GURSES <OL.GURSES@GMAIL.COM>
2024-03-02 08:20:54 +0100

    remove files model tk

--- tkinter-login-register-logout-master/app/view/tools/password_input.py
+++
@@ -1,7 +0,0 @@
-from app.view.tools.text_input import TextInput
-
-
-class PasswordInput(TextInput):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.entry.configure(show='*')
